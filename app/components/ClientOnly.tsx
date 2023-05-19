425ed10394090bbb67640d4abc0a8de902d27ce9
Sebaspa0107 <sebaspa0107@gmail.com>
2023-05-19 12:03:11 -0500

    Se crea el modal

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ClientOnlyProps {
+  children: React.ReactNode;
+}
+
+export const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) return null;
+
+  return <>{children}</>;
+};
