19373ff9caf1feff47fc6c156f1684fa527f85bd
Sebaspa0107 <sebaspa0107@gmail.com>
2023-06-30 18:28:38 -0500

    Listar mis reservaciones

---
+++ app/reservations/page.tsx
@@ -0,0 +1,41 @@
+import { ClientOnly } from "../components/ClientOnly";
+import { EmptyState } from "../components/EmptyState";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+import { ReservationsClient } from "./ReservationsClient";
+
+const page = async () => {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login." />
+      </ClientOnly>
+    );
+  }
+  const reservations = await getReservations({
+    authorId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No reservations found."
+          subtitle="You don't have any reservations."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <ReservationsClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
+};
+export default page;
