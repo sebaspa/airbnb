0c6dc3acd0dcccba75fcdb38c97757441a274453
Sebaspa0107 <sebaspa0107@gmail.com>
2023-06-30 18:35:13 -0500

    Modal de registro Abrir modal de login cuando se registra Abrir toast con el mensaje de cuando se registra

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -37,7 +37,9 @@ export const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
+        toast.success("Account created");
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
         toast.error("Something went wrong");
